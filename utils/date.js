9c93c8c34a17c8cd14476369397cedb2c4ba784b
gs0428 <lider28@naver.com>
2024-09-09 17:20:37 +0900

    feat: 오늘 날짜 포멧하는 유틸 함수 구현

---
+++ utils/date.js
@@ -0,0 +1,8 @@
+export const formatToday = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  return `${year}.${month}.${day}`;
+};
