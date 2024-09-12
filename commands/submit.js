3277c3e424be808de145189d6ac5647f6b81f7c9
gs0428 <lider28@naver.com>
2024-09-12 11:34:11 +0900

    feat: 블로그 url이 유효한지 조회하는 로직 구현

--- commands/submit.js
+++ commands/submit.js
@@ -1,5 +1,6 @@
 import { SlashCommandBuilder } from "discord.js";
 import { getValues, insertNewUser, updateSubmitDate } from "../utils/spreadsheet.js";
+import { getBlogStatus } from "../utils/blog.js";
 
 export default {
   data: new SlashCommandBuilder()
@@ -11,6 +12,20 @@ export default {
     }),
   async execute(interaction) {
     const url = interaction.options.getString("url");
+    const status = await getBlogStatus(url);
+
+    if (status === 400) {
+      return await interaction.reply({
+        content: "벨로그 혹은 티스토리 주소만 가능해요.",
+        ephemeral: true,
+      });
+    } else if (status === 404) {
+      return await interaction.reply({
+        content: "존재하지 않는 글 혹은 유저에요. 다시 한 번 확인해 주세요.",
+        ephemeral: true,
+      });
+    }
+
     const nickname = interaction.member.nickname;
     const userId = interaction.user.id;
 
---
+++ utils/blog.js
@@ -0,0 +1,12 @@
+export const getBlogStatus = async (url) => {
+  const isVelog = /^https?:\/\/(www\.)?velog.io\/@[\w.]+\/[\w\-가-힣%]+$/i.test(url);
+  const isTistory = /^https?:\/\/[\w-]+\.tistory\.com\/\d+$/.test(url);
+
+  if (!isVelog && !isTistory) {
+    return 400;
+  }
+
+  const status = await fetch(url, { method: "GET" }).then((res) => res.status);
+
+  return status;
+};
