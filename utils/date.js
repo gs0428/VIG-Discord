0937dff23551fd988a5f9c43b75169c7542442dc
gs0428 <lider28@naver.com>
2024-09-16 00:39:28 +0900

    fix: 날짜 확인하는 로직 수정

--- utils/date.js
+++ utils/date.js
@@ -8,17 +8,9 @@ export const formatToday = () => {
 };
 
 export const isInvalidSubmitDate = async (date) => {
-  // node.js는 UTC 시간대를 사용하므로 한국 시간으로 변경하기 위해 32400000ms를 더합니다.
-  // 32400000ms는 9시간을 의미합니다.
-  const submitExist = date !== "none";
-  if (!submitExist) {
-    return true;
-  }
-
   const today = new Date().setHours(0, 0, 0, 0);
   const lastSubmit = new Date(date).setHours(0, 0, 0, 0);
 
-  // 오늘 날짜와 제출한 날짜의 기간 차이가 일주일 내인지 확인합니다.
   if (today - lastSubmit <= 604800000) {
     return false;
   }
