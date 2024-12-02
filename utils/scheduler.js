a32c12fa5bfc3e8014d6f0233d8f22570ac9e029
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2024-12-02 10:58:12 +0900

    fix: 패널티 스케쥴러 로직 수정

--- utils/scheduler.js
+++ utils/scheduler.js
@@ -9,19 +9,18 @@ export const penaltyScheduler = () => {
   return cron.schedule(
     "0 0 * * 1",
     async function checkSubmitState() {
-      const [, , penaltyCounts, submitDates, activateStates] = await getValues();
+      const [ids, , penaltyCounts, submitDates, activateStates] = await getValues();
       const unsatisfiedUsers = [];
       for (let i = 1; i < submitDates.length; i++) {
         const isInvalidSubmit = await isInvalidSubmitDate(submitDates[i]);
         const isActivate = activateStates[i] === "Y";
 
         if (isInvalidSubmit && isActivate) {
-          unsatisfiedUsers.push(`<@${ids[i]}>`);
+          unsatisfiedUsers.push({ id: `<@${ids[i]}>`, index: i });
         }
       }
-
-      unsatisfiedUsers.forEach(async (userIndex) => {
-        await updatePenaltyCount(userIndex + 1, Number(penaltyCounts[userIndex]));
+      unsatisfiedUsers.forEach(async (user) => {
+        await updatePenaltyCount(user.index + 1, +penaltyCounts[user.index]);
       });
     },
     { timezone: "Asia/Seoul" }
